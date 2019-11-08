60b38a6d023b6804a5ddcb26d3c58f3463af68b1
Kerim <54900210+693941@users.noreply.github.com>
2019-11-08 09:29:35 -0500

    Edited Array games

---
+++ ker.js
@@ -0,0 +1,90 @@
+function cg(){
+	var x = prompt("Pick any year");
+	var y =(x/100)
+	var z =(Math.floor(y + 1))
+	if (z == 12){
+		alert("This year is in the" + " " + Math.floor(z) + "th Century")
+	} else if (z == 13){
+		alert("This year is in the" + " " + Math.floor(z) + "th Century")
+	} else if (z == 11){
+		alert("This year is in the" + " " + Math.floor(z) + "th Century")
+	} else if (z % 10 == 1){
+		alert("This year is in the" + " " + Math.floor(z) + "st Century")
+	} else if (z % 10 == 2){
+		alert("This year is in the" + " " + Math.floor(z) + "nd Century")
+	} else if (z % 10 == 3){
+		alert("This year is in the" + " " + Math.floor(z) + "rd Century")
+	} else {
+		alert("This year is in the" + " " + Math.floor(z) + "th Century")
+	}
+}
+
+
+function angle(){
+var x = prompt("Enter an angle belonging to a triangle (Just the Number)");
+var y = prompt("Enter the other angle of that same triangle (Just the Number)");
+
+alert(180 - x - y + " " + "degrees is the 3rd angle of your triangle");
+	}
+
+
+
+function o(){
+	var x = 0;
+	while (x <= 99){
+		x = x + 1;
+		if(x % 4 == 0 && x % 10 == 0){
+			console.log("fourten");
+		}else if(x % 10 == 0){
+			console.log("ten");
+		}else if(x % 4 == 0){
+			console.log("four");
+		}else{
+			console.log(x);
+		}
+	}
+}
+
+
+
+function length(){
+	var y = prompt("Type anything and I will tell you the length");
+	alert(y.length);
+}
+
+function Code(){
+	var x = prompt("Username")
+	var y = prompt("Password")
+	var z = x.toLowerCase()
+	if (z == "pais")
+{
+	if (z == "iscool")
+		alert("True")
+
+	else{
+		alert("False")
+	}
+	}
+}
+
+	
+function guess_number(){
+		var secret = 5 ;
+		var guess;
+		while(guess != secret){
+			guess = prompt("How old is my cat?");
+
+		if(guess == secret){
+				alert("Damn it, you got it!");
+	    }
+		else if(guess < secret){
+					alert("Guess Higher");
+   		 }
+		else if(guess > secret){
+					alert("Guess Lower");
+	 	}
+		else{
+			alert("Type in a number");
+		}
+	}
+}
